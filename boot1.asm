e91e95103f3cff1999ae17a2b96b1730040fd245
ALex Nikonov <alex+redot@nikonov.tech>
2025-02-21 02:21:47 +0700

    chore: naming things: bootN stages

--- boot1.asm
+++ boot1.asm
@@ -5,18 +5,18 @@
 ;be in memory after it is been loaded
 [ORG 0x7C00]
 
-_start_stage1:
+_start_boot0:
     cli ; clear interrupts
     ; canonicalizing CS:IP
     ; as of https://www.cs.cmu.edu/~410-s07/p4/p4-boot.pdf
     ; section 6.1
-    jmp 0x00:stage1
-stage1:
+    jmp 0x00:boot0
+boot0:
 	xor	AX, AX			;Set segment registers to zero
 	mov	ES, AX
 	mov	DS, AX
 	mov	SS, AX
-	mov	SP, _start_stage1 ;Top of stack
+	mov	SP, _start_boot0 ;Top of stack
 	mov	DI, SP			  ;is bottom of relocation point
 
     ; save the boot disk pointer in DL
@@ -33,17 +33,17 @@ stage1:
     int 0x10
 
 .main:
-    mov SI, str_hello ; SI points to the beginning of the string
+    mov SI, boot0data.str_hello ; SI points to the beginning of the string
     mov BL, 0x03      ; text color
     call    print_string
 
     ;
     ; print boot device number
     ;
-    mov si, str_bootdrive
+    mov si, boot0data.str_bootdrive
     call print_string
 
-    pop dx ; DO NOT MODIFY DX UNTIL STAGE-2
+    pop dx ; DO NOT MODIFY DX UNTIL BOOT1
     mov al, dl
     call byte_to_char
 
@@ -53,14 +53,14 @@ stage1:
     mov al, 13
     call print_chr
 
-.load_stage2:
+.load_boot1:
 .reset_drive:
     ; TODO: should I set ah=0x0d if we boot from a hard drive?
     mov ah, 0x00
     int 0x13
 
     mov	si, 3			; for i < 3, do:
-.read_stage2:
+.read_boot1:
     ; read next block from a boot disk into the memory
     mov ah, 0x02    ; read command
     mov al, 0x01    ; sector count
@@ -68,65 +68,62 @@ stage1:
     ; dx already contains the drive number we've boot from
 
     ; ES:BX = pointer to buffer
-    mov bx, stage2 ; stage1+512 must be equal to stage2 addr
+    mov bx, boot1 ; boot0+512 must be equal to boot1 addr
     int 0x13;
-	jnc	.verify_stage2			;Success
+	jnc	.verify_boot1			;Success
 
     dec si
-	jnz	.read_stage2
+	jnz	.read_boot1
 
 .load_err:
     ; handle error and hang forever
-    mov si, str_err
+    mov si, boot0data.str_err
     mov bl, 0x04 ; error color
     call print_string
     jmp $;
 
-.verify_stage2:
-    ; check the stage2 signature
-    cmp word[tab_boot1_sign], 0xDEAD
-    .t1:
+.verify_boot1:
+    cmp word[boot1data.signature], 0xDEAD
     jne .load_err
 
     ; check boot1 size, if 1 sector
     ; then we already have it loaded, jump to boot1
-    cmp byte[tab_boot1_size], 1
-    je .stage2_fully_loaded
+    cmp byte[boot1data.size_sectors], 1
+    je .boot1_fully_loaded
 
     ;
     ; size-1 = number of sectors to load
 
 .t4:
-    dec byte[tab_boot1_size]
-    mov al, byte[tab_boot1_size]
+    dec byte[boot1data.size_sectors]
+    mov al, byte[boot1data.size_sectors]
 
     mov si, 3 ; i = 3
-.load_stage2_more:
+.load_boot1_more:
     ; bootloader is larger than 1 sector, go load the rest
 
     ; read next block from a boot disk into the memory
     mov cx, 0x03  ; s1 = mbr, s2=boot1_start, read the rest starting from s3
     ; 	ES:BX = pointer to buffer
-    mov bx, stage2+512 ; stage2+512 for next sectors
+    mov bx, boot1+512 ; offset for next sectors
     mov ah, 0x02    ; read command
     ; dx already contains the drive number we've boot from
 
-.t3:
     int 0x13;
-	jnc	.stage2_fully_loaded			;Success
+	jnc	.boot1_fully_loaded			;Success
 
     ; reload al, try again
     dec si
     jz .load_err
 
-    mov al, byte[tab_boot1_size]
-	jmp	.load_stage2_more
+    mov al, byte[boot1data.size_sectors]
+	jmp	.load_boot1_more
 
-.stage2_fully_loaded:
-    mov si, str_stage2_found
+.boot1_fully_loaded:
+    mov si, boot0data.str_found
     mov BL, 0x07
     call print_string
-    jmp stage2
+    jmp boot1
 
 
 ; Assume that ASCII value is in register AL
@@ -171,7 +168,7 @@ byte_to_char:
     ; deal with a first character
     and bx, 0x00F0 ; leave only hi bytes
     shr bx, 4       ; move significant bits to the right
-    add bx, tab_hextoc ; now offset in BL, add the base address of the table
+    add bx, boot0data.tab_hextoc ; now offset in BL, add the base address of the table
     mov al, byte[bx]   ; reference a byte pointed by BX, load into AL
     mov bl, 0x07
     call print_chr ; print a byte from AL
@@ -182,7 +179,7 @@ byte_to_char:
     mov bx, ax  ; restore AX, copy into BX for modifications
     and bx, 0x000f ; remove all but LSB, got offset of the LSB sigit
     ; the rest is the same as above
-    add bx, tab_hextoc
+    add bx, boot0data.tab_hextoc
     mov al, byte[bx]
     mov bl, 0x07
     call print_chr
@@ -212,31 +209,32 @@ print_word:
    ret
 
 
-; stage1 data,
+; boot0 data,
 ; note that printing \r \n (10, 13) via bios procedures
 ; corretly move the cursor on a next line, so you don;t have to advance
 ; the cursor position by hand, which is nice.
-str_hello db 'stage1 bootloader started', 10, 13 , 0
-str_bootdrive db 'boot drive: ', 0
-str_err db 'Error loading stage2', 10, 13, 0
-str_stage2_found db 'stage2 bootloader found, passing control', 10, 13, 0
-tab_hextoc db '0123456789ABCDEF'
+boot0data:
+.str_hello db 'boot0 bootloader started', 10, 13 , 0
+.str_bootdrive db 'boot drive: ', 0
+.str_err db 'Error loading boot1', 10, 13, 0
+.str_found db 'boot1 found, passing control', 10, 13, 0
+.tab_hextoc db '0123456789ABCDEF'
 
 times 510 - ($ - $$) db 0	;fill the rest of sector with 0
-tab_boot0_sign dw 0xAA55			; add boot signature at the end of bootloader
+.mbr_signature dw 0xAA55			; add boot signature at the end of bootloader
 
 ; --- MBR end ---
 
-; stage2 starts here: the idea is to put stage2 bootloader right after the MBR,
+; boot1 starts here: the idea is to put boot1 code right after the MBR,
 ; load it via bios procedure, and pass control to a newly loaded block.
 ; offset from .ORG is 512 byte to match the size of a disk sector
-stage2:
-    mov SI, str_stage2
+boot1:
+    mov SI, boot1data.str_hello
     mov BL, 0x02
     call print_string
 
     ; print in advance, src=AX  will be trashed
-    mov SI, str_lowmem
+    mov SI, boot1data.str_lowmem
     call print_string
 
 .lowmem_detect:
@@ -253,7 +251,7 @@ stage2:
     call print_chr
 
 .set_a20:
-    mov si, str_a20_state
+    mov si, boot1data.str_a20_state
     call print_string
     call get_a20_state
     push ax
@@ -263,7 +261,7 @@ stage2:
     jmp forever
 
 .lowmem_err:
-    mov SI, str_errcode
+    mov SI, boot1data.str_errcode
     call print_string
     mov al, '1' ; how to define all codes in one place?
     call print_chr
@@ -282,7 +280,6 @@ get_a20_state:
 	push es
 
 	mov ax, 0x0000					;	0x0000:0x0500(0x00000500) -> ds:si
-    jmp forever
 	mov ds, ax
 	mov si, 0x0500
 
@@ -319,21 +316,22 @@ get_a20_state:
 .BufferBelowMB:	db 0
 .BufferOverMB	db 0
 
-; stage2 data
-str_stage2 db 'stage2 bootloader started', 10, 13, 0
-str_errcode db 'error code: ', 0
-str_lowmem db 'low mem: ', 0
-str_a20_state db 10,13,'a20: ', 0
-tab_boot1_size db 1
+; boot1 data
+boot1data:
+.str_hello db 'boot1 started', 10, 13, 0
+.str_errcode db 'error code: ', 0
+.str_lowmem db 'low mem: ', 0
+.str_a20_state db 10,13,'a20: ', 0
+.size_sectors db 1
 
-times 510 - ($ - stage2) db 0 ;; padding
-tab_boot1_sign dw 0xDEAD
+times 510 - ($ - boot1) db 0 ;; padding
+.signature dw 0xDEAD
 
 
 ; stage 3.5 test code starts below
 stage3:
     ;;; try to load this into hi mem
-    mov si, stage3.hello
+    mov si, .hello
     call print_string
     jmp $
 ; Assume that ASCII value is in register AL
@@ -359,16 +357,16 @@ stage3:
 .print_string:
     mov AL, [SI] ; chr = str[si]
     or AL, AL ; null-termintor? chr == 0 ?
-    jz stage3.return
+    jz .return
 
-    call stage3.print_chr
+    call .print_chr
 
     inc SI  ; i++
-    jmp stage3.print_string
+    jmp .print_string
 
 .return:
     ret
 
-.hello db 'stage3 loaded', 10, 13, 0
+.hello db 'boot1s2 loaded', 10, 13, 0
 times 510 - ($ - stage3) db 0 ;; padding
 dw 0xFACE
